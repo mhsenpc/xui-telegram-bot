2104afe092d01c5f6e0946f937aa0c2e7d6d2980
Mohsen Shamohammadi <mohsen.shamohammadi@pepper.com>
2024-03-17 17:17:19 +0100

    initial commit

---
+++ src/test/java/com/xui/telegram/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xui.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
