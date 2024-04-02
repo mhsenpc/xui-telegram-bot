23a3c851d1b42cc0efdb5a18901b1fd143d6efd1
Mohsen Shamohammadi <mohsen.shamohammadi@pepper.com>
2024-04-02 21:13:39 +0200

    we don't need option table because we 'll write config in a json file

--- src/main/java/com/mhsenpc/v2raybot/bot/repository/OptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mhsenpc.v2raybot.bot.repository;
-
-import com.mhsenpc.v2raybot.bot.entity.Option;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionRepository extends JpaRepository<Option, String> { }
