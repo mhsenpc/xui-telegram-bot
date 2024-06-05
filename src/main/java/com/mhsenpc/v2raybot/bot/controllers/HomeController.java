83a7e70a7114ada5b6da250dd495fd8dc78ebd20
Mohsen Shamohammadi <mohsen.shamohammadi@pepper.com>
2024-06-05 16:56:29 +0200

    add log file to project

--- src/main/java/com/mhsenpc/v2raybot/bot/controllers/HomeController.java
+++ src/main/java/com/mhsenpc/v2raybot/bot/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.mhsenpc.v2raybot.bot.controllers;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CommonsLog
 public class HomeController extends BaseController {
 
     @GetMapping("/")
