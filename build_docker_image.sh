fe11703b38870fb0fa8c9ee09c2850400d66d467
Mohsen Shamohammadi <mohsen.shamohammadi@joingsg.com>
2024-06-23 00:57:57 +0200

    add versioning for docker image builder

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
+# Get the current Git commit hash
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+# Optional: Get the current Git tag (if any) and use it as a version
+# TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+# if [ -z "$TAG" ]; then
+#   VERSION=$COMMIT_HASH
+# else
+#   VERSION=$TAG
+# fi
+
+# For simplicity, we'll just use the commit hash as the version
+VERSION=$COMMIT_HASH
+
+echo "Version: $VERSION"
+
 echo "compiling jar file"
 mvn clean package -DskipTests
 
 echo "building docker image"
-echo "pushing docker image"
-docker buildx build --platform linux/amd64,linux/arm64 -t mhsenpc/xui_bot --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t mhsenpc/xui_bot:$VERSION -t mhsenpc/xui_bot:latest .
+
+echo "pushing docker image with version $VERSION"
+docker push mhsenpc/xui_bot:$VERSION
+
+echo "pushing docker image with latest tag"
+docker push mhsenpc/xui_bot:latest
 
 echo "Done"
