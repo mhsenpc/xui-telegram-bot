5eff26b01fe91dd5acc32d1e49a5e89ebb5e5999
Mohsen Shamohammadi <mohsen.shamohammadi@joingsg.com>
2024-07-20 14:42:11 +0200

    set webhook on each run

--- src/main/java/com/mhsenpc/v2raybot/bot/CliSetWebhook.java
+++ src/main/java/com/mhsenpc/v2raybot/bot/CliSetWebhook.java
@@ -10,10 +10,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @Component
 public class CliSetWebhook implements ApplicationRunner {
 
@@ -26,40 +22,34 @@ public class CliSetWebhook implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
 
-        Path path = Paths.get("./storage/webhook.set");
-        boolean isWebhookAlreadySet = Files.exists(path);
-
-        if (args.containsOption("webhook") || !isWebhookAlreadySet) {
-
-            String token = configurationManager.getConfig(ConfigName.BOT_TOKEN);
-            if(token.isEmpty()){
-                throw new RuntimeException("Bot token was not set in config");
-            }
-
-            String botHostUrl = configurationManager.getConfig(ConfigName.BOT_HOST_URL);
-
-            if(!botHostUrl.endsWith("/")){
-                botHostUrl += "/";
-            }
-
-            SetWebhookMethodBase setWebhookMethod = new SetWebhookMethodBase();
-            setWebhookMethod.addQueryParam("url", botHostUrl + "handle");
-            setWebhookMethod.setToken(token);
-
-            try {
-                SetWebhookResponse response = requestHandler.send(setWebhookMethod, SetWebhookResponse.class);
-                Files.write(path, "1".getBytes());
-                System.out.println( "Success. webhook set. Telegram request will be sent to " + botHostUrl);;
-            }
-            catch (Exception exception){
-                System.out.println( "Failed to set webhook" + System.lineSeparator() + exception.getMessage()
-                        + "Token: " + token
-                        + "Url: " + botHostUrl);
-
-                System.out.println("Please consider check BOT_TOKEN and BOT_HOST_URL in your config!");
-
-                System.exit(0);
-            }
+        String token = configurationManager.getConfig(ConfigName.BOT_TOKEN);
+        if(token.isEmpty()){
+            throw new RuntimeException("Bot token was not set in config");
         }
+
+        String botHostUrl = configurationManager.getConfig(ConfigName.BOT_HOST_URL);
+
+        if(!botHostUrl.endsWith("/")){
+            botHostUrl += "/";
+        }
+
+        SetWebhookMethodBase setWebhookMethod = new SetWebhookMethodBase();
+        setWebhookMethod.addQueryParam("url", botHostUrl + "handle");
+        setWebhookMethod.setToken(token);
+
+        try {
+            SetWebhookResponse response = requestHandler.send(setWebhookMethod, SetWebhookResponse.class);
+            System.out.println( "Success. webhook set. Telegram request will be sent to " + botHostUrl);;
+        }
+        catch (Exception exception){
+            System.out.println( "Failed to set webhook" + System.lineSeparator() + exception.getMessage()
+                    + "Token: " + token
+                    + "Url: " + botHostUrl);
+
+            System.out.println("Please consider check BOT_TOKEN and BOT_HOST_URL in your config!");
+
+            System.exit(0);
+        }
+
     }
 }
