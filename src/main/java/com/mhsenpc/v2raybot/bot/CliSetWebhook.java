db76f7371c9b613071a83f07358adb638d29b90a
Mohsen Shamohammadi <mohsen.shamohammadi@joingsg.com>
2024-07-21 12:23:10 +0200

    don't set webhook on github runner

--- src/main/java/com/mhsenpc/v2raybot/bot/CliSetWebhook.java
+++ src/main/java/com/mhsenpc/v2raybot/bot/CliSetWebhook.java
@@ -6,6 +6,7 @@ import com.mhsenpc.v2raybot.telegram.methods.SetWebhookMethodBase;
 import com.mhsenpc.v2raybot.telegram.services.RequestHandler;
 import com.mhsenpc.v2raybot.telegram.types.SetWebhookResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -19,8 +20,15 @@ public class CliSetWebhook implements ApplicationRunner {
     @Autowired
     private ConfigurationManager configurationManager;
 
+    @Value("${github.actions:false}")
+    private boolean githubActions;
+
     @Override
     public void run(ApplicationArguments args) {
+
+        if(githubActions){
+            return;
+        }
 
         String token = configurationManager.getConfig(ConfigName.BOT_TOKEN);
         if(token.isEmpty()){
